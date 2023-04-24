a22dbc4491eed55bd2053585bd956a4215477032
bingoforjob@gmail.com <bingotyty>
2023-04-24 15:59:04 +0900

    add server cmdline

--- src/blog/cmdline.py
+++ src/blog/cmdline.py
@@ -6,3 +6,20 @@ def main(ctx, version):
         click.echo(__version__)
     elif ctx.invoked_subcommand is None:
         click.echo(ctx.get_help())
+
+@main.command()
+@click.option('-h', '--host', show_default=True, help=f'Host IP. Default: {settings.HOST}')
+@click.option('-p', '--port', show_default=True, type=int, help=f'Port. Default: {settings.PORT}')
+@click.option('--level', help='Log level')
+def server(host, port, level):
+    """Start server."""
+    kwargs = {
+        'LOGLEVEL': level,
+        'HOST': host,
+        'PORT': port,
+    }
+    for name, value in kwargs.items():
+        if value:
+            settings.set(name, value)
+
+    Server().run()
